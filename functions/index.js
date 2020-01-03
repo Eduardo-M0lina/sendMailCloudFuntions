b2996f01fc43c775063883ed0716e691dbd540a6
elmolina <eduardo.molina@farmatodo.com>
2020-01-03 13:42:20 -0500

    Implement template

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,15 @@ const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 const cors = require('cors')({origin: true});
 const config = require('./config');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const path  = require('path');
 
 /**
 * Here we're using Gmail to send 
 */
 function generateTransport(){
+    console.log("<---generateTransport--->");
     return nodemailer.createTransport({
       host: config.smtp.host,
       port: config.smtp.port,
@@ -18,8 +22,8 @@ function generateTransport(){
     })
 }
 
-function generateEmail(req) {
-    console.log("<---generateEmail--->" );
+function generateEmail(req, htmlToSend) {
+    console.log("<---generateEmail--->");
     let from = req.body.from;
     let to = req.body.to;
     let subject = req.body.subject;
@@ -32,7 +36,7 @@ function generateEmail(req) {
       from: from,
       to: to,
       subject: subject,
-      html: text
+      html: htmlToSend
     }
     /*return {
         from: 'Finaktiva <pruebas@Finaktiva.com>',
@@ -40,24 +44,32 @@ function generateEmail(req) {
         subject: '✔✔ Send Mail test ✔✔',
         html: `<h2>hola</h2>`
       }*/
-  }
+}
 
+function generateTemplate(templateName){
+    console.log("<---generateTemplate--->");
+    const filePath = path.join(__dirname, '/templates/'+templateName+'.html');
+    const source = fs.readFileSync(filePath, 'utf-8').toString();
+    const template = handlebars.compile(source);
+    const replacements = {
+        name: "Eduardo Molina"
+    };
+    return htmlToSend = template(replacements);
+}
 
 exports.sendMail = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
-        console.log("<---sendMail--->" );
+        console.log("<---sendMail--->");
         let transporter = generateTransport();
-        let mailOptions = generateEmail(req);
-        // returning result
+        let htmlToSend = generateTemplate('Email-Confirmation');
+        let mailOptions = generateEmail(req, htmlToSend);
         return transporter.sendMail(mailOptions, (erro, info) => {
             if(erro){
                 let resp = {"code": 500,"res" : "error: " + error};
                 return  res.status(500).jsonp(resp);
-                //return res.status(500).send({ msg: erro.toString() });
             }
             let resp = {"code":200,"res" : "Email sent","data" : req.body};
             return res.status(200).jsonp(resp);
-            //return res.send('Sended');
         });
     });    
 });
