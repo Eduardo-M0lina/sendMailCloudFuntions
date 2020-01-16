6f9bdf9aecca4d88a42e81723fc2a95fc28d6c40
Eduardo Molina <ing.eduardomolina04@gmail.com>
2020-01-16 14:55:14 -0500

    Update config.js

--- functions/config.js
+++ functions/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     smtp: {
-      host: 'mail.smtp2go.com',
-      port: '465',
-      user: 'liquidador',
-      password: 'NDM0eHUwaHB5M3cw'
+      host: 'xxxxx',
+      port: 'xxxxx',
+      user: 'xxxxx',
+      password: 'xxxxx'
     }
   };
